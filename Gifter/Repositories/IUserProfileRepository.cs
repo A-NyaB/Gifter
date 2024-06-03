13f4e30acbea813dc5b827de4460bbac38c47e0b
A-Nya Badger <asbadger01@gmail.com>
2024-06-03 00:11:34 -0400

    archive

---
+++ Gifter/Repositories/IUserProfileRepository.cs
@@ -0,0 +1,14 @@
+﻿using Gifter.Models;
+
+namespace Gifter.Repositories
+{
+    public interface IUserProfileRepository
+    {
+        void Add(UserProfile userprofile);
+        void Delete(int id);
+        List<UserProfile> GetAll();
+        UserProfile GetById(int id);
+        UserProfile GetUserPostsByUserId(int id);
+        void Update(UserProfile userprofile);
+    }
+}
